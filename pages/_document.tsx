cad9f946b2b904772cc7d6b89c224165b14db107
Daniel de Souza Neris <danielneris01@gmail.com>
2022-07-29 18:30:33 -0300

    vai

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,38 +1,28 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+import image from '../images/image.png';
+
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta name="title" content="DefiBank" />
+        <meta name="title" content="SmooloBetClub" />
         <meta
           name="description"
-          content="Smoolos Club is a dapp created by vertsix.com"
+          content="Smoolos Bet Club is a dapp created by skydan"
         />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://defibank.vercel.app/" />
-        <meta property="og:title" content="DefiBank" />
+        <meta
+          property="og:url"
+          content="https://smoolos-bet-club.netlify.app/"
+        />
+        <meta property="og:title" content="SmooloBetClub" />
         <meta
           property="og:description"
-          content="Smoolos Club is a dapp created by vertsix.com"
+          content="Smoolos Club is a dapp created by skydan"
         />
-        <meta
-          property="og:image"
-          content="https://defibank.vercel.app/assets/LandingPage.png"
-        />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://defibank.vercel.app/" />
-        <meta property="twitter:title" content="DefiBank" />
-        <meta
-          property="twitter:description"
-          content="Smoolos Club is a dapp created by vertsix.com"
-        />
-        <meta
-          property="twitter:image"
-          content="https://defibank.vercel.app/assets/LandingPage.png"
-        />
+        {/* <meta property="og:image" content={image} /> */}
       </Head>
       <body>
         <Main />
