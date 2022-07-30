71298c800bc7a3f7df2c4171a8ed278c4fe01635
Daniel de Souza Neris <danielneris01@gmail.com>
2022-07-30 13:28:59 -0300

    vai

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
 
   const { address } = useWeb3();
 
-  const configGames = [{ name: 'test-2', label: 'LOL TEST Game 2' }];
+  const configGames = [{ name: 'lol-1', label: 'LOL Game 1' }];
 
   const [balanceOf, setBalanceOf] = useState('0');
   const [nfts, setNfts] = useState<any[]>([]);
